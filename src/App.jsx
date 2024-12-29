31c35e3d914af7116b35169e1d1cb10c69f3c6c0
Mario Ponce <mario@mpondev.com>
2024-12-29 13:46:46 +0100

    Add media files and config Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Awards</h1>
+      <h1 className="text-5xl text-violet-300 font-bold">Awards</h1>
     </main>
   );
 }
